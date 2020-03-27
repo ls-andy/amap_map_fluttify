83944161e9fa01dbced35081ef472a58fbbe0785
yohom <yohombao@qq.com>
2020-03-27 20:36:32 +0800

    enhance: 优化demo

--- example/lib/map/tools/navi_component_screen.dart
+++ example/lib/map/tools/navi_component_screen.dart
@@ -1,4 +1,5 @@
 import 'package:amap_map_fluttify/amap_map_fluttify.dart';
+import 'package:decorated_flutter/decorated_flutter.dart';
 import 'package:flutter/material.dart';
 
 class NaviComponentScreen extends StatefulWidget {
@@ -7,20 +8,38 @@ class NaviComponentScreen extends StatefulWidget {
 }
 
 class _NaviComponentScreenState extends State<NaviComponentScreen> {
+  final from = LatLng(39.90741, 116.374016);
+  final to = LatLng(39.908068, 116.411696);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('调用导航组件')),
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            AmapService.navigateDrive(
-              from: LatLng(39.90741, 116.374016),
-              to: LatLng(39.908068, 116.411696),
-            );
-          },
-          child: Text('打开导航组件'),
-        ),
+      body: DecoratedColumn(
+        mainAxisAlignment: MainAxisAlignment.center,
+        itemSpacing: kSpaceNormal,
+        padding: EdgeInsets.symmetric(horizontal: kSpaceBig),
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          RaisedButton(
+            onPressed: () {
+              AmapService.navigateDrive(from: from, to: to);
+            },
+            child: Text('指定起点和终点, 应用内导航'),
+          ),
+          RaisedButton(
+            onPressed: () {
+              AmapService.navigateDrive(to: to);
+            },
+            child: Text('指定终点, 应用内导航'),
+          ),
+          RaisedButton(
+            onPressed: () {
+              AmapService.navigateDrive(to: to, inApp: false);
+            },
+            child: Text('指定终点, 高德app导航'),
+          ),
+        ],
       ),
     );
   }
